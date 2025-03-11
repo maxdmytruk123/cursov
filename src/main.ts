b978b7598a6fed6d9ad5adc584ca1acf55cb18f8
maxdmytruk123 <maxdmytruk02@gmail.com>
2025-03-11 17:46:45 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200', // дозволяє запити лише з цього джерела
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
